1c8332279ac0569c5ef9642137b6a077e6d4ba66
charain <charain_li@outlook.com>
2024-11-14 18:42:16 +0800

    add neo tree

---
+++ nvim/lua/plugins/utils.lua
@@ -0,0 +1,33 @@
+return {
+    -- 快捷键提示
+    { "folke/which-key.nvim"},
+    -- 括号匹配 
+    { 'echasnovski/mini.pairs', opts = {
+        mappings = {
+            ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+            ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+            ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+
+            [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+            [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+            ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+
+            ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+            ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
+            ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+        },
+    }},
+    -- 文件浏览器
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        keys = {
+            { "<leader>b", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+        },
+    }
+}
