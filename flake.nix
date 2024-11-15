95d1f89b0c22b1e9496b950413fb610a7e205b33
charain <charain_li@outlook.com>
2024-11-15 13:31:39 +0800

    update lsp & nvim

--- flake.nix
+++ flake.nix
@@ -3,18 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    crpkgs.url = "github:chai-yuan/crpkgs";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, crpkgs }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      crPkgs = crpkgs.packages.${system};
     in {
       devShells.x86_64-linux.default =
         pkgs.mkShell {
           name = "isolated-dev";
 
           buildInputs = with pkgs; [
-            neovim
+            crPkgs.neovim
+	    lua-language-server
           ];
 
           shellHook = ''
