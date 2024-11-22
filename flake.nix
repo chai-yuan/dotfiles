afd451e6f19089018d60751c57c72963cc14c239
charain <charain@debian.charain>
2024-11-22 17:38:45 -0500

    add nix lsp

--- flake.nix
+++ flake.nix
@@ -6,23 +6,31 @@
     crpkgs.url = "github:chai-yuan/crpkgs";
   };
 
-  outputs = { self, nixpkgs, crpkgs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crpkgs,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       crPkgs = crpkgs.packages.${system};
-    in {
-        packages.x86_64-linux = {
+    in
+    {
+      packages.x86_64-linux = {
 
-            my-package = pkgs.buildEnv {
-                name = "my-package-collection";
-                paths = [
-                    crPkgs.neovim
-                    pkgs.lua-language-server
-                    # Add more packages as needed
-                ];
-            };
-
+        my-package = pkgs.buildEnv {
+          name = "my-package-collection";
+          paths = [
+            crPkgs.neovim
+            pkgs.lua-language-server
+            pkgs.nixd
+            pkgs.nixfmt-rfc-style
+            pkgs.htop
+          ];
         };
+
+      };
     };
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,19 +10,23 @@ return {
 
         -- 一些快捷键配置
         -- 跳转定义和实现
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {desc = '[G]oto [D]eclaration'})
-        vim.keymap.set('n', 'gd', "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>", {desc = '[G]oto [D]efinition'})
-        vim.keymap.set('n', 'gi', "<cmd>lua require('telescope.builtin').lsp_implementations()<cr>", {desc = '[G]oto [I]mplementation'})
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, {desc = 'Type [D]efinition'})
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
+        vim.keymap.set('n', 'gd', "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>",
+            { desc = '[G]oto [D]efinition' })
+        vim.keymap.set('n', 'gi', "<cmd>lua require('telescope.builtin').lsp_implementations()<cr>",
+            { desc = '[G]oto [I]mplementation' })
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, { desc = 'Type [D]efinition' })
         -- 工作区配置
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {desc = '[W]orkspace [A]dd Folder'})
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, {desc = '[W]orkspace [R]emove Folder'})
-        vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, {desc = '[W]orkspace [L]ist Folders'})
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, { desc = '[W]orkspace [A]dd Folder' })
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, { desc = '[W]orkspace [R]emove Folder' })
+        vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+            { desc = '[W]orkspace [L]ist Folders' })
 
-        vim.keymap.set('n', '<leader>rn', "<cmd>Lspsaga rename ++project<cr>", {desc = '[R]e[n]ame'})
-        vim.keymap.set('n', '<leader>ca', "<cmd>Lspsaga code_action<CR>", {desc = '[C]ode [A]ction'})
-        vim.keymap.set('n', '<leader>da', "<cmd>lua require('telescope.builtin').diagnostics()<cr>", {desc = '[D]i[A]gnostics'})
-        vim.keymap.set('n', "<leader>fm", function() vim.lsp.buf.format {} end, {desc = "[F]ormat code"})
+        vim.keymap.set('n', '<leader>rn', "<cmd>Lspsaga rename ++project<cr>", { desc = '[R]e[n]ame' })
+        vim.keymap.set('n', '<leader>ca', "<cmd>Lspsaga code_action<CR>", { desc = '[C]ode [A]ction' })
+        vim.keymap.set('n', '<leader>da', "<cmd>lua require('telescope.builtin').diagnostics()<cr>",
+            { desc = '[D]i[A]gnostics' })
+        vim.keymap.set('n', "<leader>fm", function() vim.lsp.buf.format {} end, { desc = "[F]ormat code" })
 
         -- 各种语言服务器的配置
         lspconfig.clangd.setup {
@@ -33,6 +37,16 @@ return {
             capabilities = capabilities,
         }
         lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+        }
+        lspconfig.nixd.setup {
+            settings = {
+                nixd = {
+                    formatting = {
+                        command = { "nixfmt" },
+                    },
+                },
+            },
             capabilities = capabilities,
         }
     end
