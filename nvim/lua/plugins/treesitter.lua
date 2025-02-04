3c08273bbcd605a21415a9db6f653eec29adeab6
charain <charain_li@outlook.com>
2025-02-04 21:10:49 +0800

    add verilog treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,26 +6,26 @@ return {
     main = "nvim-treesitter.configs",
     build = ":TSUpdate",
     opts = {
-        ensure_installed = { "c", "lua", "scala" },
+        ensure_installed = { "c", "lua", "scala", "verilog" },
         highlight = {
             enable = true,
             -- 注意：这些是解析器的名称，而不是文件类型。（例如，如果你想禁用 `tex` 文件类型的语法高亮，你需要在这个列表中包含 `latex`，因为这是解析器的名称）
             -- 将被禁用的语言列表
-            disable = { "c", "rust" },
+            -- disable = { "c", "rust" },
             -- 或者使用一个函数以获得更大的灵活性，例如，禁用大文件的慢速 treesitter 高亮
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                return true
-                end
-            end,
+            -- disable = function(lang, buf)
+            --    local max_filesize = 100 * 1024 -- 100 KB
+            --    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            --    if ok and stats and stats.size > max_filesize then
+            --        return true
+            --    end
+            -- end,
 
             -- 设置为 true 将同时运行 `:h syntax` 和 tree-sitter。
             -- 如果你依赖于 'syntax' 的启用（例如用于缩进），请将此设置为 `true`。
             -- 使用此选项可能会减慢你的编辑器速度，并且你可能会看到一些重复的高亮。
             -- 除了 true，它还可以是一个语言列表
-            additional_vim_regex_highlighting = false,
+            additional_vim_regex_highlighting = true,
         },
         indent = {
             enable = true,
@@ -53,7 +53,7 @@ return {
                 -- 并且应该返回模式（'v', 'V', 或 '<c-v>'）或一个映射查询字符串到模式的表
                 selection_modes = {
                     ['@parameter.outer'] = 'v', -- 字符模式
-                    ['@function.outer'] = 'V', -- 行模式
+                    ['@function.outer'] = 'V',  -- 行模式
                     ['@class.outer'] = '<c-v>', -- 块模式
                 },
                 -- 如果你将此设置为 `true`（默认是 `false`），则任何文本对象都会
